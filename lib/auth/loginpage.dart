4bf6110826606ba32769b0c3ad90bfd0d1059604
Daniel <danieldjamilui@gmail.com>
2025-09-08 23:39:03 +0900

    first commit

---
+++ lib/auth/loginpage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page', style: TextStyle(color: Colors.white),),
+      ),
+      body: SafeArea(child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextField(decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  hint: Text("Masukkan Email"),
+                  border: OutlineInputBorder(),
+                ),),
+                TextField(),
+                TextField(decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.password),
+                  hint: Text("Password"),
+                  border: OutlineInputBorder()
+                ),),
+                SizedBox(height: 20,),
+
+                ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                }, child: Text("login"),style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity,50),
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(16)
+                  )
+                ),)
+                
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/pages/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../auth/loginpage.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('flutter Mobile Lanjutan', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.blue,
+        ),
+        body: const LoginPage(),
+      ),
+    );
+  }
+}
